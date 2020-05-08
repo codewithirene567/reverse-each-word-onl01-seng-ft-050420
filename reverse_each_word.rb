3b592ea5238a2e5995d1e7f27ed91f42d9d0c32c
Irene Scott <irenejinscott@gmail.com>
2020-05-08 17:24:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,8 +10,7 @@ def reverse_each_word(phrase)
   phrase_array = phrase.split(" ") #["Hello" "there," "and" "how" "are" "you?"]
  collected_phrase = 
  phrase_array.collect do |word|
-   
-word.reverse
+   word.reverse
 end
 phrase_array.join(' ')
 end
